ba6b41ead84998b565f655903103c80f711c6f52
Molodkina Nelly <molodkinanelli@gmail.com>
2023-02-26 22:12:05 +0300

    Добавлено решение задачи 13

--- Program.cs
+++ Program.cs
@@ -9,16 +9,30 @@ string str = Convert.ToString(num);
 Console.WriteLine(str); 
 Console.WriteLine("Вторая цифра вашего числа:  " + str[1]);
 
-// 
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+// int a = Convert.ToInt32(first);
+// int num = new Random().Next(100, 1000);
+string str1 = Convert.ToString(number);
+if (str1[2] != 0)
+Console.WriteLine("Вторая цифра вашего числа:  " + str1[2]);
+else
+Console.WriteLine("Третьей цифры нет");
+    
+
+
 // Console.WriteLine("Введите первое число: ");
 // string first = Console.ReadLine();
 // int a = Convert.ToInt32(first);
 
 
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
+
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
